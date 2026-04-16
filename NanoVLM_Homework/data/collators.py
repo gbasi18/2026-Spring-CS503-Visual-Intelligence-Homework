ec9c21444fbec8b35bb50cdd388a60891f0ea61e
Gianfranco Basile <gianfrancobasile18@gmail.com>
2026-04-16 17:06:04 +0200

    ready to run

--- NanoVLM_Homework/data/collators.py
+++ NanoVLM_Homework/data/collators.py
@@ -3,6 +3,8 @@ import torch
 class VQACollator(object):  # Visual Question Answering Collator
     def __init__(self, tokenizer, max_length):
         self.tokenizer = tokenizer
+        self.tokenizer.padding_side = "left"
+        self.tokenizer.truncation_side = "right"
         self.max_length = max_length
 
     def __call__(self, batch):
@@ -12,7 +14,7 @@ class VQACollator(object):  # Visual Question Answering Collator
         # TODO
         # Step 1 — Stack images
         # torch.stack the list of image tensors into a single batched tensor.
-        images = ...
+        images = torch.stack(images, dim=0)  
 
         # Step 2 — Build input sequences
         # The model expects a single continuous sequence per sample: f"{text}{answer}"
@@ -20,22 +22,30 @@ class VQACollator(object):  # Visual Question Answering Collator
         # Produce a list `input_sequences` of merged strings.
         input_sequences = []
         for i in range(len(texts)):
-            ...
+            input_sequence = f"{texts[i]}{answers[i]}"
+            input_sequences.append(input_sequence)
 
         # Step 3 — Batch tokenization
         # Tokenize `input_sequences` with `self.tokenizer.batch_encode_plus`.
         # Use left-padding and right-truncation up to `self.max_length`.
-        encoded_full_sequences = ...
+        encoded_full_sequences = self.tokenizer.batch_encode_plus(input_sequences,
+                                                                  padding = "max_length",
+                                                                  max_length = self.max_length,
+                                                                  #padding_side ="left",
+                                                                  truncation = True, 
+                                                                  #truncation_side = "right",
+                                                                  return_tensors = "pt")
 
         # Retrieve `input_ids` (Long tensor) and `attention_mask` from the result.
-        input_ids = ...
-        attention_mask = ...
+        input_ids = encoded_full_sequences["input_ids"]
+        attention_mask = encoded_full_sequences["attention_mask"]
 
 
         # Step 4 — Create causal labels
-        labels =  ...       # clone the input_ids
-        ... # in a causal LM, the target at position t is the token at position t+1
-        ...  # make sure, no target (label = -100) for the very last position
+        labels = torch.full_like(input_ids, -100)      # clone the input_ids
+        labels[:,:-1] = input_ids[:,1:] # in a causal LM, the target at position t is the token at position t+1
+        
+        # make sure, no target (label = -100) for the very last position
 
         # Step 5 — Per-sample label masking
         # Even though we created labels for the input_ids, we need to mask the loss calculation for tokens that belongs to pad positions, and for the examples that were truncated
@@ -48,24 +58,33 @@ class VQACollator(object):  # Visual Question Answering Collator
         # If the full text is shorter than the max length, we need to set the labels to -100 only for the question part, and create causal language modeling labels for the answer part, taking into account the padding
 
         # First, compute the *untruncated* `original_lengths` token length for every input sequence
-        original_lengths = ...
+        original_lengths = self.tokenizer(input_sequences,
+                                                padding=False,
+                                                truncation=False,
+                                                return_length=True,
+                                            )["length"]
         # Then iterate over each sample i of the batch and handle two cases:
         for i in range(len(batch)):
             # Get the length of the question for this sample
-            question_length = ...
+            question_length = len(self.tokenizer.encode(texts[i],
+                                                    padding = False,
+                                                    truncation = False,
+                                                    add_special_tokens=False))
 
 
             # case A: If a sequence was truncated (original is longer than max_length)
                 # Set all labels to -100 to ignore this sample entirely,
-            ...
+            if original_lengths[i] > self.max_length:
+                labels[i,:] = -100
             # Case B: (else) sequence fits within max_length (left-padded)
             # The tokenizer left-pads short sequences, so find where non-padding tokens begin (using attention_mask)
-
-            first_token_pos = ... # the first 1 (non-zero value) in the attention mask for a given batch sample marks the first non-padding token
-            # using first_token_pos and question_length to find the position where the question ends. Also, because labels are already shifted left by 1 relative to input_ids,
-            # take into account the left shift by subtracting 1)
-            question_end = ...
-            ... # update labels for padding and question part to -100
+            else:
+                 
+                first_token_pos = attention_mask[i].argmax().item() # the first 1 (non-zero value) in the attention mask for a given batch sample marks the first non-padding token
+                # using first_token_pos and question_length to find the position where the question ends. Also, because labels are already shifted left by 1 relative to input_ids,
+                # take into account the left shift by subtracting 1)
+                question_end = first_token_pos + question_length - 1
+                labels[i,:question_end] = -100 # update labels for padding and question part to -100
 
         return {
             "image": images,
